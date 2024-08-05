319b56a2c380c3c98697c0cbae7054162446804d
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-08-06 03:04:11 +0530

    pass 1st stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	// Uncomment this line to pass the first stage
 	// "encoding/json"
 	"fmt"
@@ -44,18 +45,17 @@ func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		// Uncomment this block to pass the first stage
-		//
-		// bencodedValue := os.Args[2]
-		//
-		// decoded, err := decodeBencode(bencodedValue)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	return
-		// }
-		//
-		// jsonOutput, _ := json.Marshal(decoded)
-		// fmt.Println(string(jsonOutput))
+
+		bencodedValue := os.Args[2]
+
+		decoded, err := decodeBencode(bencodedValue)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		jsonOutput, _ := json.Marshal(decoded)
+		fmt.Println(string(jsonOutput))
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
